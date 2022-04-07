adf3e02f97daca4e215fa77ddf8ba2eda720e220
Dalton Hall <dhall.07113@gmail.com>
2022-04-07 14:42:32 -0500

    Dalton

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 
+
 const App = () => {
     return (
         <BrowserRouter>
