212b1f1d7f31c2bc144f24feb5dddc4d3c3157c5
WasabiAddict <davidtltran@gmail.com>
2022-04-07 16:21:46 -0500

    added nav bar

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx"
 import Home from "./pages/Home.jsx";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Navbar />
+
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
--- client/index.jsx
+++ client/index.jsx
@@ -5,5 +5,6 @@ import '@babel/polyfill';
 import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "./styles.css"
 
 render(<App />, document.getElementById("root"));
